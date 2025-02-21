d9e8733df9f0491cfc08447c88397eddcc5e3261
Tiia Nylén <58226295+Tiiawss@users.noreply.github.com>
2025-02-21 17:51:06 +0200

    Update game_test.py

--- harjoitustyo2048/tests/game_test.py
+++ harjoitustyo2048/tests/game_test.py
@@ -1,7 +1,7 @@
 import unittest
 from base_game.game import Game2048
 from main import get_random_move, run_game
-from algorithm.minimax import minimax_algorithm
+from algorithm.minimax import minimax_algorithm, smoothness, monotonicity
 import numpy as np
 
 
@@ -43,12 +43,18 @@ class TestGame2048(unittest.TestCase):
 
         for _ in range(6):
             if play_turn % 2 == 0:
-
                 direction = get_random_move()
                 move_sequence.append("random")
             else:
 
-                direction = minimax_algorithm(self.testGame)
+                direction, _ = minimax_algorithm(
+                    self.testGame,
+                    depth=3,
+                    max_depth=3,
+                    maximizing=True,
+                    alpha=-10000,
+                    beta=10000
+                )
                 move_sequence.append("minimax algorithm")
 
             self.testGame.make_move(direction)
@@ -93,3 +99,42 @@ class TestGame2048(unittest.TestCase):
         expected_matrix = self.testGame.matrix.copy()
         self.testGame.push_tiles()
         np.testing.assert_array_equal(self.testGame.matrix, expected_matrix)
+
+    def test_smoothness_on_smooth_board(self):
+        """Test smoothness function on smooth board"""
+        self.game = Game2048()
+        self.game.matrix = np.array([
+            [2, 4, 8, 16],
+            [32, 64, 128, 256],
+            [512, 1024, 2048, 4096],
+            [8192, 16384, 32768, 65536]
+        ])
+
+        score = smoothness(self.game)
+        self.assertLess(score, 0)
+
+    def test_monotonicity_on_monotonic_board(self):
+        """Test monotonicity on a completely monotonic board"""
+        self.game = Game2048()
+        self.game.matrix = np.array([
+            [1024, 512, 256, 128],
+            [64, 32, 16, 8],
+            [4, 2, 1, 0],
+            [0, 0, 0, 0]
+        ])
+        score = monotonicity(self.game)
+        self.assertLess(score, 0)
+
+    def test_minimax_with_clear_best_move(self):
+        """Test minimax when there is a clear best move"""
+        self.game = Game2048()
+        self.game.matrix = np.array([
+            [2, 4, 8, 16],
+            [32, 64, 128, 256],
+            [512, 1024, 2048, 4096],
+            [0, 0, 0, 0]
+        ])
+        best_move, _ = minimax_algorithm(
+            self.game, depth=3, max_depth=3, maximizing=True, alpha=-float('inf'), beta=float('inf'))
+        self.assertEqual(best_move, "left")
+
